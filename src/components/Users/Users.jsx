7b9216ef77be13df4f73ee89e05bd5335054c0ae
boxing-in-iT <tbol240@gmail.com>
2024-03-17 13:50:57 +0200

    Fix

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,4 +1,3 @@
-// Users.js
 import React, { useContext, useEffect, useState } from "react";
 import "./index.scss";
 import UserCard from "../UserCard/UserCard";
@@ -45,74 +44,3 @@ const Users = () => {
 };
 
 export default Users;
-
-// import React, { useContext, useEffect, useState } from "react";
-// import "./index.scss";
-// import UserCard from "../UserCard/UserCard";
-// import { UserUpdateContext } from "../../App";
-
-// const Users = () => {
-//   const [users, setUsers] = useState([]);
-//   const [page, setPage] = useState(1);
-//   const [hasMore, setHasMore] = useState(true);
-
-//   const { userUpdated } = useContext(UserUpdateContext);
-
-//   const fetchUsers = () => {
-//     debugger;
-//     fetch(
-//       `https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         if (data.success) {
-//           if (data.users.length < 6) {
-//             setHasMore(false);
-//           }
-//           if (page === 1) {
-//             setUsers(data.users);
-//           } else {
-//             setUsers((prevUsers) => [...prevUsers, ...data.users]);
-//           }
-//         } else {
-//           console.error("Server error:", data.message);
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//       });
-//   };
-
-//   useEffect(() => {
-//     setPage(1);
-//     fetchUsers();
-//   }, [userUpdated]);
-
-//   useEffect(() => {
-//     fetchUsers();
-//   }, [page]);
-
-//   const handleShowMore = () => {
-//     setPage(page + 1);
-//   };
-
-//   return (
-//     <section>
-//       <h2>Working with GET request</h2>
-//       <div className="cards">
-//         {users.map((userData, i) => (
-//           <UserCard key={userData.id} user={userData} />
-//         ))}
-//       </div>
-//       {hasMore && (
-//         <div className="button-section">
-//           <button className="button" onClick={handleShowMore}>
-//             Show more
-//           </button>
-//         </div>
-//       )}
-//     </section>
-//   );
-// };
-
-// export default Users;
