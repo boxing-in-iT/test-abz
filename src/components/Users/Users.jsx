6b1dab8e039ed559073f2dd1d29f4af6c6ea7da8
boxing-in-iT <tbol240@gmail.com>
2024-03-17 13:15:59 +0200

    Fix

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,9 @@
+// Users.js
 import React, { useContext, useEffect, useState } from "react";
 import "./index.scss";
 import UserCard from "../UserCard/UserCard";
 import { UserUpdateContext } from "../../App";
+import fetchUsers from "../../helper";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -10,44 +12,13 @@ const Users = () => {
 
   const { userUpdated } = useContext(UserUpdateContext);
 
-  console.log("USERUPFA: ", userUpdated);
-
-  const fetchUsers = () => {
-    fetch(
-      `https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          if (data.users.length < 6) {
-            setHasMore(false);
-          }
-          // Если это первая страница, очищаем пользователей
-          if (page === 1) {
-            console.log("PAGE1");
-            setUsers(data.users);
-          } else {
-            console.log("AFTER");
-            setUsers((prevUsers) => [...prevUsers, ...data.users]);
-          }
-        } else {
-          console.error("Server error:", data.message);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  };
-
   useEffect(() => {
-    console.log("SetPAge effect");
-    setPage(1);
-    fetchUsers();
+    page === 1 ? fetchUsers(page, setUsers, setPage, setHasMore) : setPage(1);
   }, [userUpdated]);
 
   useEffect(() => {
-    console.log("Fetch effect");
-    fetchUsers();
+    console.log("SSS");
+    fetchUsers(page, setUsers, setPage, setHasMore);
   }, [page]);
 
   const handleShowMore = () => {
@@ -74,3 +45,74 @@ const Users = () => {
 };
 
 export default Users;
+
+// import React, { useContext, useEffect, useState } from "react";
+// import "./index.scss";
+// import UserCard from "../UserCard/UserCard";
+// import { UserUpdateContext } from "../../App";
+
+// const Users = () => {
+//   const [users, setUsers] = useState([]);
+//   const [page, setPage] = useState(1);
+//   const [hasMore, setHasMore] = useState(true);
+
+//   const { userUpdated } = useContext(UserUpdateContext);
+
+//   const fetchUsers = () => {
+//     debugger;
+//     fetch(
+//       `https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         if (data.success) {
+//           if (data.users.length < 6) {
+//             setHasMore(false);
+//           }
+//           if (page === 1) {
+//             setUsers(data.users);
+//           } else {
+//             setUsers((prevUsers) => [...prevUsers, ...data.users]);
+//           }
+//         } else {
+//           console.error("Server error:", data.message);
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//       });
+//   };
+
+//   useEffect(() => {
+//     setPage(1);
+//     fetchUsers();
+//   }, [userUpdated]);
+
+//   useEffect(() => {
+//     fetchUsers();
+//   }, [page]);
+
+//   const handleShowMore = () => {
+//     setPage(page + 1);
+//   };
+
+//   return (
+//     <section>
+//       <h2>Working with GET request</h2>
+//       <div className="cards">
+//         {users.map((userData, i) => (
+//           <UserCard key={userData.id} user={userData} />
+//         ))}
+//       </div>
+//       {hasMore && (
+//         <div className="button-section">
+//           <button className="button" onClick={handleShowMore}>
+//             Show more
+//           </button>
+//         </div>
+//       )}
+//     </section>
+//   );
+// };
+
+// export default Users;
--- src/helper.js
+++ src/helper.js
@@ -1,28 +1,26 @@
-export const fetchUsers = (page, setUsers, setPage, setHasMore) => {
-  fetch(
-    `https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success) {
-        if (data.users.length < 6) {
-          setHasMore(false);
-        }
-        if (page === 1) {
-          // Если это первая страница, просто устанавливаем новый список пользователей
-          setUsers(data.users);
-        } else {
-          // Если это не первая страница, добавляем новых пользователей к текущему списку
-          setUsers((prevUsers) => [...prevUsers, ...data.users]);
-        }
-        setPage(page + 1); // Увеличиваем номер страницы для следующего запроса
-      } else {
-        console.error("Server error:", data.message);
+// api.js
+export const fetchUsers = async (page, setUsers, setPage, setHasMore) => {
+  try {
+    const response = await fetch(
+      `https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`
+    );
+    const data = await response.json();
+
+    if (data.success) {
+      if (data.users.length < 6) {
+        setHasMore(false);
       }
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
+      if (page === 1) {
+        setUsers(data.users);
+      } else {
+        setUsers((prevUsers) => [...prevUsers, ...data.users]);
+      }
+    } else {
+      console.error("Server error:", data.message);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 export default fetchUsers;
